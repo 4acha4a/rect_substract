38ef2ba0ac436cc16008eb356c9efe20a1565711
Daniil Kirillov <danik.mad@gmail.com>
2023-12-13 19:36:56 +0300

    main function edited

--- main.c
+++ main.c
@@ -26,6 +26,29 @@ void draw_rectangle(rect_t rect, char xy[][PLANE_SIZE]) {
 } //Draws a specific rectangle on a coordinate plane
 
 int main() {
-    test_rect();
+    uint8_t src_size = 100;
+    uint8_t src_cnt = 1;
+    rect_t* src = (rect_t*)malloc(src_size * sizeof(rect_t));
+    src[0] = (rect_t){0, 0, 32, 24};
+    char xy[PLANE_SIZE][PLANE_SIZE];
+    for (uint16_t x = 0; x < PLANE_SIZE; ++x) {
+        for (uint16_t y = 0; y < PLANE_SIZE; ++y) {
+            xy[x][y] = '-';
+        }
+    }
+    rect_t initial = src[0];
+    for (uint16_t x = initial.x; x <= initial.x + initial.w; ++x) {
+        for (uint16_t y = initial.y; y <= initial.y + initial.h; ++y) {
+            rects_substract(src, src_size, &src_cnt, (rect_t){x,y,1,1});
+        }
+    }
+    for (uint8_t i = 0; i < src_size; ++i) {
+        if(is_null_rect(src[i]))
+            continue;
+        print_rect(src[i]);
+    }
+    draw(xy);
+    free(src);
+    printf("%d\n", src_cnt);
     return 0;
 }
