175febac29551fc7a264b619943256669bb1049f
Daniil Kirillov <danik.mad@gmail.com>
2023-12-27 22:12:31 +0300

    + few more tests

--- test_rect.c
+++ test_rect.c
@@ -30,8 +30,64 @@ void test_rect_fuzzy() {
     }
     printf("\n");
     for (size_t i = 0; i < src_cnt; ++i) {
-        if (is_null_rect(src[i]))
-            continue;
+        print_rect(src[i]);
+    }
+    free(src);
+}
+
+void test_rect_max_count() {
+    printf("x; y; w; h\n");
+    const uint8_t src_size = 10;
+    uint8_t src_cnt = 1;
+    rect_t* src = (rect_t*)malloc(src_size * sizeof(rect_t));
+    rect_t first_rect = {0, 0, 1000, 1000};
+    src[0] = first_rect;
+
+    rect_t s1 = {300, 300, 400, 400};
+    rects_substract(src, src_size, &src_cnt, s1);
+    print_rect(s1);
+
+    rect_t s2 = {400, 100, 200, 100};
+    rects_substract(src, src_size, &src_cnt, s2);
+    print_rect(s2);
+
+    rect_t s3 = {400, 800, 200, 100};
+    rects_substract(src, src_size, &src_cnt, s3);
+    print_rect(s3);
+
+    printf("\n");
+    for (size_t i = 0; i < src_cnt; ++i) {
+        print_rect(src[i]);
+    }
+    free(src);
+} // PASS
+
+void test_rect_null_rects() {
+    printf("x; y; w; h\n");
+    const uint8_t src_size = 100;
+    uint8_t src_cnt = 1;
+    rect_t* src = (rect_t*)malloc(src_size * sizeof(rect_t));
+    rect_t first_rect = {0, 0, 1000, 1000};
+    src[0] = first_rect;
+
+    rect_t s1 = {300, 300, 400, 400};
+    rects_substract(src, src_size, &src_cnt, s1);
+    print_rect(s1);
+
+    rect_t s2 = {400, 100, 200, 100};
+    rects_substract(src, src_size, &src_cnt, s2);
+    print_rect(s2);
+
+    rect_t s3 = {400, 800, 200, 100};
+    rects_substract(src, src_size, &src_cnt, s3);
+    print_rect(s3);
+
+    rect_t big = {0, 0, 500, 1000};
+    rects_substract(src, src_size, &src_cnt, big);
+    print_rect(big);
+
+    printf("\n");
+    for (size_t i = 0; i < src_cnt; ++i) {
         print_rect(src[i]);
     }
     free(src);
@@ -60,14 +116,40 @@ void test_rect_hito() {
     rects_substract(src, src_size, &src_cnt, send_btn);
     print_rect(send_btn);
     for (size_t i = 0; i < src_cnt; ++i) {
-        if (is_null_rect(src[i]))
-            continue;
         print_rect(src[i]);
     }
     free(src);
-}
+} // PASS
+
+void test_rect_hito_negative() {
+    printf("x; y; w; h\n");
+    const uint8_t src_size = 100;
+    uint8_t src_cnt = 1;
+    rect_t* src = (rect_t*)malloc(src_size * sizeof(rect_t));
+    rect_t hito_screen = {0, 0, -450, -350};
+    src[0] = hito_screen;
+    rect_t battery = {-430, -5, -18, -10};
+    rects_substract(src, src_size, &src_cnt, battery);
+    print_rect(battery);
+    rect_t more = {-10, -10, -50, -20};
+    rects_substract(src, src_size, &src_cnt, more);
+    print_rect(more);
+    rect_t etherium_logo = {-70, -30, -320, -240};
+    rects_substract(src, src_size, &src_cnt, etherium_logo);
+    print_rect(etherium_logo);
+    rect_t receive_btn = {-20, -320, -190, -20};
+    rects_substract(src, src_size, &src_cnt, receive_btn);
+    print_rect(receive_btn);
+    rect_t send_btn = {-220, -320, -190, -20};
+    rects_substract(src, src_size, &src_cnt, send_btn);
+    print_rect(send_btn);
+    for (size_t i = 0; i < src_cnt; ++i) {
+        print_rect(src[i]);
+    }
+    free(src);
+} //PASS
 
 void test_rect() {
+    // test_rect_null_rects();
     test_rect_fuzzy();
-    // test_rect_hito();
 }
